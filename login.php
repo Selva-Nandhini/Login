918b206bf9f4e3f495a3a96ab8fa7c9e00db7a39
NANDHINI <nandhinisns12@gmail.com>
2023-03-24 20:01:09 +0530

    updated the files

--- login.php
+++ login.php
@@ -1,21 +1,22 @@
 <?php
 
+include "datatable.php";
 $uname = $_POST['uname'];
 $password = $_POST['password'];
 
 
-$con = new mysqli("localhost", "root", "", "login form");
+$con = new mysqli("localhost", "root", "", "contactform");
 if($con->connect_error) {
     die("Failed to connect : " .$con->connect_error);
 } else {
-    $stmt = $con->prepare("select * from user where uname = ?");
+    $stmt = $con->prepare("select * from users where uname = ?");
     $stmt->bind_param("s", $uname);
     $stmt->execute();
     $stmt_result = $stmt->get_result();
     if($stmt_result->num_rows > 0) {
         $data = $stmt_result->fetch_assoc();
        if($data['password'] === $password) {
-       echo "<h1> LOGIN SUCCESSFULLY </h1>";
+           header ("Location:datatable.php");
        } 
     } else {
         echo "<h1>INVALID USERNAME OR PASSWORD";
